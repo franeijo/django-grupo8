f697eafa73f86d29af068c7577b5380687e3054d
franeijo <franeijo@gmail.com>
2023-04-15 16:52:50 -0300

    Agregando Segunda Vista

--- proyecto_grupo8/app/views.py
+++ proyecto_grupo8/app/views.py
@@ -3,3 +3,7 @@ from django.http import HttpResponse
 
 def home(request):
     return render(request, "home.html")
+
+def reservas(request):
+    parameters = ("Usuario","Reserva 1")
+    return render(request, "reservas.html", {"parameters": parameters})
--- proyecto_grupo8/proyecto_grupo8/urls.py
+++ proyecto_grupo8/proyecto_grupo8/urls.py
@@ -20,4 +20,5 @@ from app.views import *
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('home/', home),
+    path('reservas/', reservas),
 ]
