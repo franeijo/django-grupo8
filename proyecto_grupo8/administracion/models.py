5260dd64466c4693193c5b73ef1841399617c1e7
dfw-sys <dfw.sistemas@gmail.com>
2023-05-13 21:30:19 -0300

    Se actualizo settings, incorporo models, Notas agregadas

--- proyecto_grupo8/administracion/models.py
+++ proyecto_grupo8/administracion/models.py
@@ -1,3 +1,106 @@
 from django.db import models
 
-# Create your models here.
+class Edificios(models.Model):
+    id_edif = models.AutoField(primary_key=True)
+    pais = models.CharField(max_length=100)
+    pcia = models.CharField(max_length=100)
+    ciudad = models.CharField(max_length=100)
+    nombre = models.CharField(max_length=100, null=True)
+    direccion = models.CharField(max_length=150)
+
+    class Meta:
+        db_table = "edificios"
+
+
+class UnidadFuncional(models.Model):
+    TIPO_UF = [
+        (1,'Departamento'),
+        (2,'Cochera'),
+        (3,'Local'),
+        (4,'Otro'),
+    ]
+    id_uf = models.AutoField(primary_key=True)
+    unidad_funcional = models.CharField(max_length=4)
+    uf_tipo = models.IntegerField(choices=TIPO_UF,default=1)
+    piso = models.CharField(max_length=10)
+    dpto = models.CharField(max_length=5)
+    coprop_nombre = models.CharField(max_length=150)
+    porc_a = models.CharField(max_length=2, null=True)
+    porc_b = models.CharField(max_length=2, null=True)
+    porc_c = models.CharField(max_length=2, null=True)
+    porc_d = models.CharField(max_length=2, null=True)
+    edif_fk = models.ForeignKey(Edificios, on_delete=models.RESTRICT, related_name="unidad_funcional")
+
+    class Meta:
+        db_table = "unidad_funcional"
+
+
+class Amenities(models.Model):
+    id_ameni = models.AutoField(primary_key=True)
+    descripcion = models.CharField(max_length=255, null=True)
+
+    class Meta:
+        db_table = "amenities"
+
+
+class EdifAmeni(models.Model):
+    edi_fk = models.ForeignKey(Edificios, on_delete=models.RESTRICT, related_name="edif_ameni")
+    ameni_fk = models.ForeignKey(Amenities, on_delete=models.RESTRICT, related_name="edif_ameni")
+
+    class Meta:
+        db_table = "edif_ameni"
+
+
+class ReservaAmeni(models.Model):
+    id_reserva = models.AutoField(primary_key=True)
+    fecha_dde = models.DateField()
+    hora_dde = models.TimeField()
+    fecha_hasta = models.DateField()
+    hora_hasta = models.TimeField()
+    edi_fk = models.ForeignKey(Edificios, on_delete=models.RESTRICT, related_name="reserva_ameni")
+    uf_fk = models.ForeignKey(UnidadFuncional, on_delete=models.RESTRICT, related_name="reserva_ameni")
+    ameni_fk = models.ForeignKey(Amenities, on_delete=models.RESTRICT, related_name="reserva_ameni")
+
+    class Meta:
+        db_table = "reserva_ameni"
+
+
+class ReclamosSugerencias(models.Model):
+    RECLAMO_SUG = [
+        (1,'Suegerencia'),
+        (2,'Reclamo'),
+        (3,'Felicitaciones'),
+        (4,'Otro'),
+    ]
+
+    id_rec_sug = models.AutoField(primary_key=True)
+    tipo_rec_sug = models.IntegerField(choices=RECLAMO_SUG,default=1)
+    asunto = models.CharField(max_length=100, null=True)
+    descripcion = models.CharField(max_length=255, null=True)
+    edi_fk = models.ForeignKey(Edificios, on_delete=models.RESTRICT, related_name="reclamos_sugerencias")
+    uf_fk = models.ForeignKey(UnidadFuncional, on_delete=models.RESTRICT, related_name="reclamos_sugerencias", null=True)
+
+    class Meta:
+        db_table = "reclamos_sugerencias"
+		
+		
+class Gastos(models.Model):
+    TIPO_GASTO = [
+        (1,'Arreglos'),
+        (2,'Abonos'),
+        (3,'Sueldos'),
+        (4,'Insumos'),
+        (5,'Suplencias'),
+        (6,'Extraordinarios'),
+        (7,'Otros'),
+    ]
+
+    id_gastos = models.AutoField(primary_key=True)
+    tipo_gasto = models.IntegerField(choices=TIPO_GASTO,default=1)
+    concepto = models.CharField(max_length=255)
+    importe = models.FloatField()
+    id_edificio_fk = models.ForeignKey(Edificios, on_delete=models.CASCADE, db_column='id_edificio_fk')
+
+    class Meta:
+        db_table = 'gastos'
+        
--- proyecto_grupo8/proyecto_grupo8/settings.py
+++ proyecto_grupo8/proyecto_grupo8/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
+import os
 from pathlib import Path
 import mimetypes
 import environ
@@ -117,11 +118,13 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.0/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'en-AR'
 
 TIME_ZONE = 'UTC'
 
 USE_I18N = True
+
+USE_L10N = True
 
 USE_TZ = True
 
